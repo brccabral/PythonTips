8730c054dc79d96a7e9aa717731d265321ba3a69
Bruno Cabral <cabral.brc@gmail.com>
2021-11-27 18:41:54 -0800

    string Template

--- 009_string_formatting.py
+++ 009_string_formatting.py
@@ -2,6 +2,20 @@ name = 'my name'
 subs = 9
 
 # don't use +
-msg = "Wow! " + name + " you have " + subs + " subs!"
+msg = "Wow! " + name + " you have " + str(subs) + " subs!"
 # use f""
 msg = f"Wow! {name} you have {subs} subs!"
+
+
+
+from string import Template
+template = Template("The table ${tablename} has ${rows} rows")
+
+result = template.substitute(tablename='ELEMENT', rows=4)
+print(result)
+
+data = {'tablename':'ELEMENT', 'rows': 4}
+result = template.substitute(data)
+print(result)
+
+# result = template.substitute('ELEMENT', 4) # doesn't work
